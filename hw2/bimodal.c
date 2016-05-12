800d48d2c6cc5e1f74d49582fdad3456a2b7cafa
Chad Spensky <cspensky@cs.ucsb.edu>
2016-05-11 17:53:07 -0700

    Adding hw2 answers

---
+++ hw2/bimodal.c
@@ -0,0 +1,4 @@
+for (i = 1 to infinity) {
+	if (i > 3 && (i%2 == 1 || i == 4))
+		/* Do something awesome */
+}
---
+++ hw2/bimodal_2.c
@@ -0,0 +1,4 @@
+for (i = 1 to infinity) {
+	if (i > 5 && (i%6 ==0  || (i-1)%6 == 0 || (i-2)%6 == 0))
+		/* Do something awesome */
+}
